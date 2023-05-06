c9064bca48e8ebcc26d3483a40fe7ecf8edaea0d
yladev <yanlinnaung881@gmail.com>
2023-05-07 02:50:48 +0630

    update light mode and dark mode change functionality

--- js/script.js
+++ js/script.js
@@ -39,3 +39,13 @@ menuBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     sidebar.style.display = 'none';
 })
+
+// theme change
+const themeBtn = document.querySelector('.theme-btn');
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+
+    themeBtn.querySelector('span:first-child').classList.toggle('active');
+    themeBtn.querySelector('span:last-child').classList.toggle('active');
+})
